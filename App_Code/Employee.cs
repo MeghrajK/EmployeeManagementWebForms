af972d0d1e71d7fac5023212eca0ce36adebf317
Meghraj Kanhere <Meghrajkanhere007@outlook.com>
2025-07-06 08:11:02 +0530

    Add project files.

---
+++ App_Code/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EmployeeManagementWebForms
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+        public string FormattedSalary => Salary.ToString("C");
+        public string FormattedHireDate => HireDate.ToString("MM/dd/yyyy");
+    }
+}
